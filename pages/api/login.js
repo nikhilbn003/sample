a426f81f013846294b66550b95ad4b871745de19
nikhilbn <nikhilbn003@gmail.com>
2023-04-26 16:31:29 +0530

    api

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,17 +3,17 @@ import  User from '../../models/login';
 
 connect()
 
-export default async function handle(req,res){
-    const{email, password } = req.body
-    const user = await User.findOne({email,password})
-    if(!user){
-        return res.json({status:'not able to find user .'})
-    }
-    else{
-        res.redirect('/checkout')
-    }
+// export default async function handle(req,res){
+//     const{email, password } = req.body
+//     const user = await User.findOne({email,password})
+//     if(!user){
+//         return res.json({status:'not able to find user .'})
+//     }
+//     else{
+//         res.redirect('/checkout')
+//     }
 
-}
+// }
 
 
 
