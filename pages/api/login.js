0f8d2da9984084e6cb173e5234ac66c72f19c03a
nikhilbn <nikhilbn003@gmail.com>
2023-04-26 16:35:33 +0530

    ap

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,37 +3,37 @@ import  User from '../../models/login';
 
 connect()
 
-// export default async function handle(req,res){
-//     const{email, password } = req.body
-//     const user = await User.findOne({email,password})
-//     if(!user){
-//         return res.json({status:'not able to find user .'})
-//     }
-//     else{
-//         res.redirect('/checkout')
-//     }
+export default async function handle(req,res){
+    const{email, password } = req.body
+    const user = await User.findOne({email,password})
+    if(!user){
+        return res.json({status:'not able to find user .'})
+    }
+    else{
+        res.redirect('/checkout')
+    }
 
-// }
-
-
-
-import { connectToDatabase } from './connect';
-
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { name, email, message } = req.body;
-
-    const client = await connectToDatabase();
-    const db = client.db();
-
-    const result = await db.collection('messages').insertOne({
-      name,
-      email,
-      message,
-    });
-
-    console.log(result);
-
-    res.status(201).json({ message: 'Message sent!' });
-  }
 }
+
+
+
+// import { connectToDatabase } from './connect';
+
+// export default async function handler(req, res) {
+//   if (req.method === 'POST') {
+//     const { name, email, message } = req.body;
+
+//     const client = await connectToDatabase();
+//     const db = client.db();
+
+//     const result = await db.collection('messages').insertOne({
+//       name,
+//       email,
+//       message,
+//     });
+
+//     console.log(result);
+
+//     res.status(201).json({ message: 'Message sent!' });
+//   }
+// }
